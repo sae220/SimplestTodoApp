d938108db7becd0a1f843ed41e31289ca25a3d8e
sae220 <mod-sharp.truetle3@outlook.jp>
2024-01-18 11:39:57 +0900

    データベースとの接続について追加

---
+++ app/src/database.py
@@ -0,0 +1,19 @@
+from os import environ as env
+from typing import Annotated, Generator
+
+from fastapi import Depends
+from sqlmodel import create_engine, Session, SQLModel
+
+
+DATABASE_URL = f'postgresql://{env["POSTGRES_USER"]}:{env["POSTGRES_PASSWORD"]}@db:5432/{env["POSTGRES_DB"]}'
+
+engine = create_engine(DATABASE_URL)
+
+def init_database() -> None:
+    SQLModel.metadata.create_all(engine)
+
+def get_database_session() -> Generator[:,:,Session]:
+    with Session(engine) as session:
+        yield session
+
+DatabaseSessionGetter = Annotated[Session, Depends(get_database_session)]
--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
 
+from .database import init_database
+
+
+init_database()
 
 app = FastAPI()
