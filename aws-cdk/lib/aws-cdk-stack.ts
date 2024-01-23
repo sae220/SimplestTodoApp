2f1f112e4830c19b738463031cfb7e2633830f93
sae220 <mod-sharp.truetle3@outlook.jp>
2024-01-23 09:00:11 +0900

    余計な部分を消して大まかな流れをコメント

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC・サブネット・セキュリティグループの作成
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BlogQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // RDSの作成
+
+    // ECRの作成・Dockerイメージをプッシュ
+
+    // App Runner作成・独自ドメインの設定
   }
 }
