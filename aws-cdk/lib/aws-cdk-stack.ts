e99eba58f56bd2e64a10a86ea15d0f734f9fd18a
sae220 <mod-sharp.truetle3@outlook.jp>
2024-01-24 09:38:35 +0900

    cdkのvpc部分を作成

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -1,11 +1,24 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+
+const PROJECT_NAME = 'simplestTodoApp'
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // VPC・サブネット・セキュリティグループの作成
+    const vpc = new ec2.Vpc(this, `${PROJECT_NAME}-vpc`, {
+      maxAzs: 2
+    })
+    const securityGroupForAppRunner = new ec2.SecurityGroup(this, `${PROJECT_NAME}-sg-forApprunner`, {
+      vpc
+    })
+    const securityGroupForRDS = new ec2.SecurityGroup(this, `${PROJECT_NAME}-sg-forRds`, {
+      vpc
+    })
+    securityGroupForRDS.addIngressRule(securityGroupForAppRunner, ec2.Port.tcp(5432))
 
     // RDSの作成
 
