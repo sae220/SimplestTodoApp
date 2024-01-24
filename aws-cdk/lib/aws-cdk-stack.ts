1f346215f6c43ffaa82e9807dcb955d59bb2b490
sae220 <mod-sharp.truetle3@outlook.jp>
2024-01-24 11:30:29 +0900

    cdkのecrとapprunner部分を作成

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -1,7 +1,11 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as ecr from 'aws-cdk-lib/aws-ecr'
 import * as rds from 'aws-cdk-lib/aws-rds'
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager'
+import * as apprunner from '@aws-cdk/aws-apprunner-alpha'
+import * as image_deploy from 'cdk-docker-image-deployment'
 
 const PROJECT_NAME = 'simplestTodoApp'
 const POSTGRES_USER = 'testuser'
@@ -36,7 +40,32 @@ export class AwsCdkStack extends cdk.Stack {
     })
 
     // ECRの作成・Dockerイメージをプッシュ
+    const ecrRepository = new ecr.Repository(this, `${PROJECT_NAME}-ecr`)
+    new image_deploy.DockerImageDeployment(this, `${PROJECT_NAME}-imagedeploy`, {
+      source: image_deploy.Source.directory('../app'),
+      destination: image_deploy.Destination.ecr(ecrRepository)
+    })
 
     // App Runner作成・独自ドメインの設定
+    const vpcConnector = new apprunner.VpcConnector(this, `${PROJECT_NAME}-vpcconnector`, {
+      vpc,
+      securityGroups: [securityGroupForAppRunner]
+    })
+    new apprunner.Service(this, `${PROJECT_NAME}-apprunner`, {
+      source: apprunner.Source.fromEcr({
+        repository: ecrRepository,
+        imageConfiguration: {
+          environmentVariables: {
+            POSTGRES_USER,
+            POSTGRES_DB
+          },
+          environmentSecrets: {
+            POSTGRES_PASSWORD: apprunner.Secret.fromSecretsManager(secretsmanager.Secret.fromSecretNameV2(this, `${PROJECT_NAME}-db-password`, `${PROJECT_NAME}-db-password`))
+          },
+          port: 8000
+        }
+      }),
+      vpcConnector
+    })
   }
 }
